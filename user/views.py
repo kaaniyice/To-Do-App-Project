076ccd8aeee480b92b51734547accb2cbe7e4a07
Kaan İyice <kaan35iyice@gmail.com>
2023-12-22 17:09:14 +0300

    refactor: add logout function

--- user/views.py
+++ user/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import login as LoginUser
+
 
 def register(request):
     if request.method == 'POST':
@@ -22,7 +22,7 @@ def register(request):
         # If want to have email change None to email and get the value via email = request.POST.get('user_email')
         try:
             user = User.objects.create_user(username, None, password)
-            return redirect('user:login')
+            return HttpResponseRedirect(reverse("user:login"))
         except Exception as error:
             # Handle registration errors
             return render(request, 'user/register.html', {'error': str(error)})
@@ -32,7 +32,7 @@ def register(request):
         return render(request, 'user/register.html')
 
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -41,7 +41,7 @@ def login(request):
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            LoginUser(request, user)
+            login(request, user)
             return HttpResponseRedirect(reverse("todo:index"))
         else:
             # Invalid credentials
@@ -49,3 +49,8 @@ def login(request):
     else:
         # Render the login form
         return render(request, 'user/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("user:login"))
