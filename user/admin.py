0e4dc9e845c375a05ef66b9416c52d58cd201f4a
Kaan İyice <kaan35iyice@gmail.com>
2023-12-23 21:40:14 +0300

    feat: create Task Setting admin model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from user.models import *
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'description', 'deadline', 'priority', 'done', 'updated_date', 'created_date']
+    search_fields = ['user', 'description', 'deadline']
+    list_editable = ['description','deadline','done']
+
+    class Meta:
+        model = Task
