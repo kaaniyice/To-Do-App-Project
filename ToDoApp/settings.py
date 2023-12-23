1f59554c1bb309d246a36604a63167c01fa70e89
Kaan İyice <kaan35iyice@gmail.com>
2023-12-24 00:26:32 +0300

    refactor: integrate template to login and register page

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import environ
+import os
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 env = environ.Env(Debug=(bool, False))
 environ.Env.read_env()
@@ -123,6 +127,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
