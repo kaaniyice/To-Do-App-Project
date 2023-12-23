7daf15bd779328a84250b0ec813e91cf939f347e
Kaan İyice <kaan35iyice@gmail.com>
2023-12-23 19:22:23 +0300

    feat: create models for Task, change add remove functions

--- todo/views.py
+++ todo/views.py
@@ -2,42 +2,49 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django import forms
+from user.models import Task
 
 
 # Create your views here.
 @login_required
 def index(request):
-    if "tasks" not in request.session or request.session["tasks"] is None:
-        request.session["tasks"] = []
-    return render(request, "todo/index.html", {
-        "tasks": request.session["tasks"]
-    })
+    tasks = Task.objects.filter(user=request.user).order_by('deadline')
+    return render(request, "todo/index.html", {"tasks": tasks})
 
 
 @login_required
 def add(request):
-    request.session.modified = True
     if request.method == "POST":
-        task = request.POST["add_name"]
-        date = request.POST["add_date"]
-        try:
-            request.session["tasks"] += [task]
-        except:
-            pass
+        description = request.POST.get("description")
+        deadline = request.POST.get("deadline")  # Get other fields as needed
+        priority = request.POST.get("priority")
+        # Manual validation (or use model validation methods for convenience)
+        if not description:
+            # Handle missing description
+            return render(request, "todo/index.html", {"error": "Description is required"})
+        # Create task instance and assign user
+        task = Task(
+            user=request.user,
+            description=description,
+            deadline=deadline,  # Set other fields as needed
+            priority=priority
+        )
+        # Validate using model methods
+
+        task.save()
+        return HttpResponseRedirect(reverse("todo:index"))
     return HttpResponseRedirect(reverse("todo:index"))
 
 
 @login_required
 def remove(request):
     if request.method == "POST":
-        name = request.POST.get("task_value", "")
-        if name in request.session["tasks"]:
-            # The django session object can only save when its modified. But because you are modifying an object within
-            # session, the session object doesn't know its being modified and hence it cant save.
-            # To let the session object know its modified use:
-            # request.session.modified = True
-            request.session.modified = True
-            request.session["tasks"].remove(name)
-            print(request.session["tasks"])
-    return HttpResponseRedirect(reverse("todo:index"))
+        task_id = request.POST.get("task_id")
+        # Retrieve task by description and check ownership
+        task = Task.objects.get(id=task_id, user=request.user)
+        if request.method == "POST":
+            task.delete()
+            return HttpResponseRedirect(reverse("todo:index"))  # Redirect to the index page
+        else:
+            # Handle GET request if needed (e.g., confirmation page)
+            return render(request, "todo/confirm_remove.html", {"task": task})
--- user/models.py
+++ user/models.py
@@ -1,3 +1,79 @@
+from django.core.validators import MinValueValidator
+from datetime import datetime, timedelta
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+PRIORITY_CHOICES = (
+    ('low', 'Low'),
+    ('medium', 'Medium'),
+    ('high', 'High'),
+)
+
+
+class AbstractModel(models.Model):
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='Updated Date',
+        help_text='',
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='Created Date',
+        help_text='',
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Task(AbstractModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='User',
+        help_text='',
+    )
+    description = models.CharField(
+        default='',
+        max_length=254,
+        blank=False,
+        verbose_name='Description',
+        help_text='',
+    )
+    deadline = models.DateField(
+        blank=True,
+        verbose_name='Due Date',
+        help_text='',
+        validators=[MinValueValidator(datetime.now() + timedelta(days=1)), ]
+    )
+
+    priority = models.CharField(
+        max_length=6,
+        choices=PRIORITY_CHOICES,
+        default='medium',
+    )
+    done = models.BooleanField(
+        default=False,
+        verbose_name='Done',
+        help_text='',
+    )
+
+    def extend_deadline(self, days):
+        self.deadline += timedelta(days=days)
+        self.save()
+
+    def change_done_status(self):
+        if self.done:
+            self.done = False
+        else:
+            self.done = True
+
+    def __str__(self):
+        return f'Task Setting: {self.description}'
+
+    class Meta:
+        verbose_name = 'Task Setting'
+        verbose_name_plural = 'Task Settings'
+        ordering = ('-created_date',)
