7ea3c77d4e2bb15e1abee2e59cbbe6859a5af226
Kaan İyice <kaan35iyice@gmail.com>
2023-12-23 22:28:43 +0300

    refactor: Change templates files dir

--- todo/views.py
+++ todo/views.py
@@ -9,7 +9,7 @@ from user.models import Task
 @login_required
 def index(request):
     tasks = Task.objects.filter(user=request.user).order_by('deadline')
-    return render(request, "todo/index.html", {"tasks": tasks})
+    return render(request, "index.html", {"tasks": tasks})
 
 
 @login_required
@@ -21,19 +21,22 @@ def add(request):
         # Manual validation (or use model validation methods for convenience)
         if not description:
             # Handle missing description
-            return render(request, "todo/index.html", {"error": "Description is required"})
+            return render(request, "index.html", {"error": "Description is required"})
         # Create task instance and assign user
-        task = Task(
-            user=request.user,
-            description=description,
-            deadline=deadline,  # Set other fields as needed
-            priority=priority
-        )
+        try:
+            task = Task(
+                user=request.user,
+                description=description,
+                deadline=deadline,  # Set other fields as needed
+                priority=priority
+            )
+            task.save()
+        except Exception as error:
+            return render(request, "index.html", {"error": error})
         # Validate using model methods
-
-        task.save()
         return HttpResponseRedirect(reverse("todo:index"))
-    return HttpResponseRedirect(reverse("todo:index"))
+    else:
+        return HttpResponseRedirect(reverse("todo:index"))
 
 
 @login_required
@@ -42,9 +45,9 @@ def remove(request):
         task_id = request.POST.get("task_id")
         # Retrieve task by description and check ownership
         task = Task.objects.get(id=task_id, user=request.user)
-        if request.method == "POST":
+        if task:
             task.delete()
             return HttpResponseRedirect(reverse("todo:index"))  # Redirect to the index page
         else:
             # Handle GET request if needed (e.g., confirmation page)
-            return render(request, "todo/confirm_remove.html", {"task": task})
+            return HttpResponseRedirect(reverse("todo:index"))
--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
 
@@ -13,12 +13,12 @@ def register(request):
         # Ensure username and password are not empty
         if not username or not password:
             # Handle empty fields
-            return render(request, 'user/register.html', {'error': 'Please fill in all fields.'})
+            return render(request, 'register.html', {'error': 'Please fill in all fields.'})
 
         # Check for existing user
         if User.objects.filter(username=username).exists():
             # Handle username already exists
-            return render(request, 'user/register.html', {'error': 'Username already exists.'})
+            return render(request, 'register.html', {'error': 'Username already exists.'})
 
         # If want to have email change None to email and get the value via email = request.POST.get('user_email')
         if password == password2:
@@ -27,13 +27,13 @@ def register(request):
                 return HttpResponseRedirect(reverse("user:login"))
             except Exception as error:
                 # Handle registration errors
-                return render(request, 'user/register.html', {'error': str(error)})
+                return render(request, 'register.html', {'error': str(error)})
         else:
-            return render(request, 'user/register.html', {'error': "Passwords don't match"})
+            return render(request, 'register.html', {'error': "Passwords don't match"})
 
     else:
         # Render the registration form
-        return render(request, 'user/register.html')
+        return render(request, 'register.html')
 
 
 def login_view(request):
@@ -45,16 +45,16 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if not username or not password:
             # Handle empty fields
-            return render(request, 'user/login.html', {'error': 'Please fill in all fields.'})
+            return render(request, 'login.html', {'error': 'Please fill in all fields.'})
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("todo:index"))
         else:
             # Invalid credentials
-            return render(request, 'user/login.html', {'error': 'Invalid username or password'})
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
     else:
         # Render the login form
-        return render(request, 'user/login.html')
+        return render(request, 'login.html')
 
 
 def logout_view(request):
